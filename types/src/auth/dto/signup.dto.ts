e65e7891f034b80a6ac2ec6f29fdf3c2baa909ff
Jacques Nguyen <jacques@cadeaudelamaison.com>
2025-07-21 21:22:04 +0200

    simplify pwd rules

--- types/src/auth/dto/signup.dto.ts
+++ types/src/auth/dto/signup.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsString, MinLength, MaxLength, Matches } from 'class-validator';
+import { IsEmail, IsString, MinLength, MaxLength } from 'class-validator';
 
 export class SignupDto {
   @IsString()
@@ -12,10 +12,6 @@ export class SignupDto {
 
   @IsString()
   @MinLength(8, { message: 'Password must be at least 8 characters long' })
-  @MaxLength(30, { message: 'Password cannot be more than 30 characters long' })
-  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
-    message: 'Password too weak',
-  })
   password!: string;
 
   @IsString()
